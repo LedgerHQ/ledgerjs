06ff8300d02f0a013cdc5fb511aed3c4fd55a3f1
Maxime <maxime.tissier@gmail.com>
2017-05-19 21:09:52 +0200

    paused tests between apps

--- test/tests-node.js
+++ test/tests-node.js
@@ -9,28 +9,44 @@ else {
     comm = ledger.comm_u2f;
 }
 
+var Q = require('q');
+
 var TIMEOUT = 20;
 
 var tests = [
-    {name:'testBtc' , test: require('./testBtc')},
-    {name:'testBtc2' , test: require('./testBtc2')},
-    {name:'testBtc3' , test: require('./testBtc3')},
-    {name:'testBtc4' , test: require('./testBtc4')},
-    {name:'testEth' , test: require('./testEth')},
-    {name:'testEth2' , test: require('./testEth2')},
-    {name:'testEth3' , test: require('./testEth3')},
-    {name:'testEth4' , test: require('./testEth4')}
+    {name: 'testBtc', run: require('./testBtc')},
+    {name: 'testBtc2', run: require('./testBtc2')},
+    {name: 'testBtc3', run: require('./testBtc3')},
+    {name: 'testBtc4', run: require('./testBtc4')},
+    {
+        run: function () {
+            var deferred = Q.defer();
+            var s = 15;
+            console.info('You have ' + s + ' seconds to switch to eth app ...');
+            var interval = setInterval(function () {
+                if (--s) {
+                    console.log(s + ' ...');
+                } else {
+                    clearInterval(interval);
+                    deferred.resolve();
+                }
+            }, 1000);
+            return deferred.promise;
+        }
+    },
+    {name: 'testEth', run: require('./testEth')},
+    {name: 'testEth2', run: require('./testEth2')},
+    {name: 'testEth3', run: require('./testEth3')},
+    {name: 'testEth4', run: require('./testEth4')}
 ];
 
-var Q = require('q');
-
 function runTests() {
-    tests.reduce(function (a, test) {
+    tests.reduce(function (a, step) {
         return a.then(function () {
-            console.warn('Running test', test.name);
-            return (test.test)(comm, ledger, TIMEOUT);
+            console.info(step.name ? 'Running test ' + step.name : '');
+            return (step.run)(comm, ledger, TIMEOUT);
         }).fail(function (err) {
-            console.error('Failed test', test.name, err);
+            console.error('Failed test', step.name, err);
         })
     }, Q.resolve());
 }
