e34d45873d6521d08284f20123cb0baccbe0559c
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2018-01-11 19:30:47 +0100

    write basic node example

--- packages/example-node/src/index.js
+++ packages/example-node/src/index.js
@@ -1,1 +1,11 @@
-// TODO
+import TransportNodeHid from "@ledgerhq/hw-transport-node-hid";
+import AppBtc from "@ledgerhq/hw-app-btc";
+
+async function example() {
+  const transport = await TransportNodeHid.create(5000, true);
+  const appBtc = new AppBtc(transport);
+  const o = await appBtc.getWalletPublicKey("44'/0'/0'/0");
+  console.log("getWalletPublicKey:", o);
+}
+
+example();
