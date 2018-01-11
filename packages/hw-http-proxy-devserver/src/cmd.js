2cdfb6ab6ad4647cbf7bd88d25c760b957113e52
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2018-01-11 19:37:15 +0100

    wip on two new libs

---
+++ packages/hw-http-proxy-devserver/src/cmd.js
@@ -0,0 +1,35 @@
+import TransportNodeHid from "@ledgerhq/hw-transport-node-hid";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+var app = express();
+
+const PORT = process.env.PORT || 8435;
+
+TransportNodeHid.create(5000, process.env.DEBUG || false).then(comm => {
+  app.use(cors());
+
+  let pending = false;
+  app.post("/", bodyParser.json(), async (req, res) => {
+    if (!req.body) return res.sendStatus(400);
+    console.log("BODY", req.body);
+    let data = null,
+      error = null;
+    if (pending) {
+      res.status(400).json({ error: "an exchange query was already pending" });
+    }
+    pending = true;
+    try {
+      data = await comm.exchange(req.body.apduHex, req.body.statusList);
+    } catch (e) {
+      error = e.toString();
+    }
+    pending = false;
+    res.json({ data, error });
+  });
+
+  app.listen(PORT, () => {
+    console.log("hw-transport-http-proxy-debug listening on " + PORT + "...");
+  });
+});
---
+++ packages/hw-transport-http/src/HttpTransport.js
@@ -0,0 +1,46 @@
+//@flow
+import Transport from "@ledgerhq/hw-transport";
+
+/**
+ * HTTP transport implementation
+ */
+export default class HttpTransport extends Transport {
+  static async create(): Promise<Transport> {
+    throw "HttpProxyTransport.create not available";
+  }
+
+  url: string;
+
+  /**
+   * The full url of an http server.
+   * * a GET is expected to return a 200 status on this url.
+   * * a POST is expected to accept { apduHex, statusList } object in body and
+       returns { data, error } where either data is filled with an hex result
+       or error is filled with a text error.
+   * @example
+   * new HttpTransport("http://192.168.0.1/")
+   */
+  constructor(url: string) {
+    super();
+    this.url = url;
+  }
+
+  async exchange(apduHex: string, statusList: Array<number>): Promise<string> {
+    const response = await fetch(this.url, {
+      method: "POST",
+      body: JSON.stringify({ apduHex, statusList })
+    });
+    if (response.status !== 200) {
+      throw "failed to communicate to server. code=" + response.status;
+    }
+    const body = await response.json();
+    if (body.error) throw body.error;
+    return body.data;
+  }
+
+  setScrambleKey() {}
+
+  close(): Promise<void> {
+    return Promise.resolve();
+  }
+}
