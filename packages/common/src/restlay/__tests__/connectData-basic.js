c039d7113b71b287dfa5fe18d61c335cf492a840
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2018-01-23 14:57:00 +0100

    fix test without snapshot

--- packages/common/src/restlay/__tests__/connectData-basic.js
+++ packages/common/src/restlay/__tests__/connectData-basic.js
@@ -9,7 +9,7 @@ test("the mock-1 and utility functions works", () => {
   const net = networkFromMock(createMock());
   const render = createRender(net.network);
   const inst = renderer.create(render(<div />));
-  expect(inst.toJSON()).toMatchSnapshot();
+  expect(inst.toJSON()).toEqual(renderer.create(render(<div />)).toJSON());
   expect(net.countTickWaiters()).toBe(0);
   expect(net.tick()).toBe(0);
   inst.unmount();
@@ -32,11 +32,7 @@ test("a simple query works", async () => {
       const { animals } = this.props;
       return (
         <div>
-          {animals.map(a => (
-            <div key={a.id}>
-              {a.name}: {a.age}
-            </div>
-          ))}
+          {animals.map(a => <div key={a.id}>{a.name + ": " + a.age}</div>)}
         </div>
       );
     }
@@ -47,11 +43,22 @@ test("a simple query works", async () => {
     }
   });
   const inst = renderer.create(render(<Animals />));
-  expect(inst.toJSON()).toMatchSnapshot();
+  expect(inst.toJSON()).toBe(null);
   expect(net.tick()).toBe(1);
   await flushPromises();
   expect(net.countTickWaiters()).toBe(0);
-  expect(inst.toJSON()).toMatchSnapshot();
+  expect(inst.toJSON()).toEqual(
+    renderer
+      .create(
+        render(
+          <div>
+            <div>max: 14</div>
+            <div>doge: 5</div>
+          </div>
+        )
+      )
+      .toJSON()
+  );
   inst.unmount();
 });
 
@@ -61,11 +68,7 @@ test("query with props works", async () => {
   class AnimalR extends Component<*> {
     render() {
       const { a } = this.props;
-      return (
-        <div key={a.id}>
-          {a.name}: {a.age}
-        </div>
-      );
+      return <div key={a.id}>{a.name + ": " + a.age}</div>;
     }
   }
   const Animal = connectData(AnimalR, {
@@ -75,15 +78,19 @@ test("query with props works", async () => {
     propsToQueryParams: ({ id_prop }) => ({ animalId: id_prop })
   });
   const inst = renderer.create(render(<Animal id_prop="id_max" />));
-  expect(inst.toJSON()).toMatchSnapshot();
+  expect(inst.toJSON()).toEqual(null);
   expect(net.tick()).toBe(1);
   await flushPromises();
   expect(net.countTickWaiters()).toBe(0);
-  expect(inst.toJSON()).toMatchSnapshot();
+  expect(inst.toJSON()).toEqual(
+    renderer.create(render(<div>max: 14</div>)).toJSON()
+  );
   inst.update(render(<Animal id_prop="id_doge" />));
   expect(net.tick()).toBe(1);
   await flushPromises();
   expect(net.countTickWaiters()).toBe(0);
-  expect(inst.toJSON()).toMatchSnapshot();
+  expect(inst.toJSON()).toEqual(
+    renderer.create(render(<div>doge: 5</div>)).toJSON()
+  );
   inst.unmount();
 });
