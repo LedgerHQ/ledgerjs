850da12c113535ead6d05632be84d500c0995955
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2018-01-23 13:19:36 +0100

    add documentation-website

---
+++ packages/documentation-website/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    // HACK FOR NOW
+    return (
+      <iframe
+        src="/docs"
+        frameborder="0"
+        title="docs"
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          border: "none",
+          width: "100%",
+          height: "100%"
+        }}
+      />
+    );
+  }
+}
+
+export default App;
