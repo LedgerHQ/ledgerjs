d89adeeb931a2cda59965921a2735acce3235a30
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2018-01-23 13:50:54 +0100

    ignore some flow for now

--- packages/hw-transport-http/src/withStaticURL.js
+++ packages/hw-transport-http/src/withStaticURL.js
@@ -1,19 +1,18 @@
 // @flow
-
 import HttpTransport from "./HttpTransport";
-export default (urlArg: ?string) => {
+export default (urlArg: ?string): Class<HttpTransport> => {
   const url = urlArg;
   if (!url) return HttpTransport; // by default, HttpTransport don't yield anything in list/listen
   class StaticHttpTransport extends HttpTransport {
     static list = (): * => HttpTransport.open(url).then(() => [url], () => []);
     static listen = (observer: *) => {
       let unsubscribed = false;
-
       function attemptToConnect() {
         if (unsubscribed) return;
         HttpTransport.open(url, 5000).then(
           () => {
             if (unsubscribed) return;
+            // $FlowFixMe wtf flow
             observer.next({ type: "add", descriptor: url });
             observer.complete();
           },
