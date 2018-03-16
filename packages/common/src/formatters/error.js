b87130f8a1ab74dd121e76a6a9b9793eddcd5149
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2018-03-16 16:34:26 +0100

    bump deps

--- packages/common/src/formatters/error.js
+++ packages/common/src/formatters/error.js
@@ -1,8 +1,8 @@
 //@flow
 
-export default (error: ?Error | string) => {
+export default (error: mixed) => {
   if (typeof error === "string") return error;
-  const genStr = ((error && error.message) || "").toString();
+  const genStr = String((error && error.message) || "");
   // u2f-api lib https://github.com/grantila/u2f-api/blob/1e75b41fd5d9d001e6ad2be2eda6b9b41d137a81/lib/u2f-api.js#L98
   if (error && typeof error.metaData === "object" && error.metaData) {
     const metaData = error.metaData;
