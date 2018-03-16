e34cadba6afae294ac8372e8257b96effd202f23
Thibaut Boustany <thibaut@ledger.fr>
2018-03-16 11:39:54 +0100

    Cointype definition unification

---
+++ packages/currencies/src/coinTypeToIconName.js
@@ -0,0 +1,8 @@
+// @flow
+export default {
+  "0": "bitcoin",
+  "1": "bitcoin",
+  "2": "litecoin",
+  "3": "dogecoin",
+  "5": "dash"
+};
--- packages/currencies/src/reactNative.js
+++ packages/currencies/src/reactNative.js
@@ -1,16 +1,9 @@
 // @flow
 import * as icons from "./data/icons/reactNative";
-
-const iconsByCoinType = {
-  "0": icons.bitcoin,
-  "1": icons.bitcoin,
-  "2": icons.litecoin,
-  "3": icons.dogecoin,
-  "5": icons.dash
-};
+import coinTypeToIconName from "./coinTypeToIconName";
 
 type Icon = React$ComponentType<{ size: number, color: string }>;
 
 export function getIconByCoinType(coinType: number): ?Icon {
-  return iconsByCoinType[coinType];
+  return icons[coinTypeToIconName[coinType]];
 }
