333ade0d55dc9c59bcc4b451cf7c976e78629681
Loëck Vézien <nainpuissant@gmail.com>
2018-03-29 11:20:59 +0200

    Check usagePage for Win/OSX and interface for Linux

--- packages/hw-transport-node-hid/src/isLedgerDevice.js
+++ packages/hw-transport-node-hid/src/isLedgerDevice.js
@@ -1,4 +1,6 @@
-// We check the usagePage for get only HID device
+// We check the usagePage on Win/OSX, and interface on Linux for get only HID device
 export default d =>
-  d.usagePage === 0xffa0 &&
+  (["win32", "darwin"].includes(process.platform)
+    ? d.usagePage === 0xffa0
+    : d.interface === 0) &&
   ((d.vendorId === 0x2581 && d.productId === 0x3b7c) || d.vendorId === 0x2c97);
