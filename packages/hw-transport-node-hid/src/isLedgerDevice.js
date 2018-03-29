f300c9ea3286f9282f2d996adfb4fbba483037f7
Loëck Vézien <nainpuissant@gmail.com>
2018-03-29 10:13:24 +0200

    Fix issue for listen Devices with 1.4 firmware

--- packages/hw-transport-node-hid/src/isLedgerDevice.js
+++ packages/hw-transport-node-hid/src/isLedgerDevice.js
@@ -1,3 +1,4 @@
-export default device =>
-  (device.vendorId === 0x2581 && device.productId === 0x3b7c) ||
-  device.vendorId === 0x2c97;
+// We check the usagePage for get only HID device
+export default d =>
+  d.usagePage === 0xffa0 &&
+  ((d.vendorId === 0x2581 && d.productId === 0x3b7c) || d.vendorId === 0x2c97);
