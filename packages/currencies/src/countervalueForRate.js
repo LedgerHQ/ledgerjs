005d58f22e10e6e30e29eea9e531628348bef32c
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2018-04-03 12:29:19 +0200

    deprecate some functions to be moved out

--- packages/currencies/src/countervalueForRate.js
+++ packages/currencies/src/countervalueForRate.js
@@ -4,6 +4,9 @@ import type { Rate, UnitValue } from "./types";
 
 // calculate the counter value at a specific rate
 export default (rate: Rate, value: number): UnitValue => {
+  console.log(
+    "DEPRECATED: countervalueForRate is deprecated. this logic needs to move out of currencies. for instance in wallet-common"
+  );
   const unit = getFiatUnit(rate.fiat);
   return {
     value: Math.round(rate.value * value),
