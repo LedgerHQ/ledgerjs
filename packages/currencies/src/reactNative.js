542548eafc33ffc189973d334d96a4561934037a
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2018-06-07 09:21:15 +0200

    Drop @ledgerhq/common and @ledgerhq/currencies
    we have moved to https://github.com/LedgerHQ/ledger-live-common/tree/master/src

--- packages/currencies/src/reactNative.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import * as icons from "./data/icons/reactNative";
-import coinTypeToIconName from "./coinTypeToIconName";
-
-type Icon = React$ComponentType<{ size: number, color: string }>;
-
-export function getIconByCoinType(coinType: number): ?Icon {
-  return icons[coinTypeToIconName[coinType]];
-}
