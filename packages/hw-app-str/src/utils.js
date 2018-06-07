ecf8f757a88f24bf1801e2a3ad9d4cb560c5953e
phra <greensoncio@gmail.com>
2018-06-07 12:15:02 +0200

    chore: fix prettier linting

--- packages/hw-app-str/src/utils.js
+++ packages/hw-app-str/src/utils.js
@@ -59,15 +59,15 @@ export function crc16xmodem(buf: Buffer, previous?: number): number {
 
   for (var index = 0; index < buf.length; index++) {
     const byte = buf[index];
-    let code = crc >>> 8 & 0xFF;
+    let code = (crc >>> 8) & 0xff;
 
-    code ^= byte & 0xFF;
+    code ^= byte & 0xff;
     code ^= code >>> 4;
-    crc = crc << 8 & 0xFFFF;
+    crc = (crc << 8) & 0xffff;
     crc ^= code;
-    code = code << 5 & 0xFFFF;
+    code = (code << 5) & 0xffff;
     crc ^= code;
-    code = code << 7 & 0xFFFF;
+    code = (code << 7) & 0xffff;
     crc ^= code;
   }
 
