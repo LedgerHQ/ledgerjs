96c1e3555435ed011a13d96e50fe6aab746528a1
Thibaut Boustany <thibaut@kastor.fr>
2018-07-30 17:21:58 +0200

    Restore interface filtering

--- packages/hw-transport-node-hid/src/getDevices.js
+++ packages/hw-transport-node-hid/src/getDevices.js
@@ -1,6 +1,11 @@
 // @flow
 import HID from "node-hid";
 
+const filterInterface = device =>
+  ["win32", "darwin"].includes(process.platform)
+    ? device.usagePage === 0xffa0
+    : device.interface === 0;
+
 export default function getDevices(): Array<*> {
-  return HID.devices(0x2c97, 0x0);
+  return HID.devices(0x2c97, 0x0).filter(filterInterface);
 }
