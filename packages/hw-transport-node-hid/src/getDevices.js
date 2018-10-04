b75bbf251e5f47d83476f8fd6309fe0607a9401b
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2018-10-04 07:45:00 +0200

    Update some dependency, fix flow

--- packages/hw-transport-node-hid/src/getDevices.js
+++ packages/hw-transport-node-hid/src/getDevices.js
@@ -3,9 +3,11 @@ import HID from "node-hid";
 
 const filterInterface = device =>
   ["win32", "darwin"].includes(process.platform)
-    ? device.usagePage === 0xffa0
+    ? // $FlowFixMe bug in HID flow def
+      device.usagePage === 0xffa0
     : device.interface === 0;
 
 export default function getDevices(): Array<*> {
+  // $FlowFixMe bug in HID flow def
   return HID.devices(0x2c97, 0x0).filter(filterInterface);
 }
