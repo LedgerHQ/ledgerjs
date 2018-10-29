85d907fcbb4b1665014be00e1c95599a6650d171
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2018-10-29 20:11:22 +0100

    hw-transport-http: More generic type for withStaticURLS

--- packages/hw-transport-http/src/withStaticURLs.js
+++ packages/hw-transport-http/src/withStaticURLs.js
@@ -11,44 +11,60 @@ import type {
 const getTransport = url =>
   !url.startsWith("ws") ? HttpTransport : WebSocketTransport;
 
-export default (urls: string[]): Class<Transport<string>> => {
+type InS = string | string[];
+type InP = Promise<InS> | InS;
+type In = InP | (() => InP);
+
+const inferURLs = async (urls: In): Promise<string[]> => {
+  const r = await (typeof urls === "function" ? urls() : urls);
+  return typeof r === "string" ? [r] : r;
+};
+
+export default (urls: In): Class<Transport<string>> => {
   class StaticTransport extends Transport<string> {
     static isSupported = HttpTransport.isSupported;
 
     static list = (): Promise<*[]> =>
-      Promise.all(
-        urls.map(url =>
-          getTransport(url)
-            .check(url)
-            .then(() => [url])
-            .catch(() => [])
+      inferURLs(urls)
+        .then(urls =>
+          Promise.all(
+            urls.map(url =>
+              getTransport(url)
+                .check(url)
+                .then(() => [url])
+                .catch(() => [])
+            )
+          )
         )
-      ).then(arrs => arrs.reduce((acc, a) => acc.concat(a), []));
+        .then(arrs => arrs.reduce((acc, a) => acc.concat(a), []));
 
     static listen = (observer: Observer<DescriptorEvent<*>>): Subscription => {
       let unsubscribed = false;
       const seen = {};
       function checkLoop() {
         if (unsubscribed) return;
-        Promise.all(
-          urls.map(async url => {
-            if (unsubscribed) return;
-            try {
-              await getTransport(url).check(url);
-              if (unsubscribed) return;
-              if (!seen[url]) {
-                seen[url] = 1;
-                observer.next({ type: "add", descriptor: url });
-              }
-            } catch (e) {
-              // nothing
-              if (seen[url]) {
-                delete seen[url];
-                observer.next({ type: "remove", descriptor: url });
-              }
-            }
-          })
-        )
+        inferURLs(urls)
+          .then(urls =>
+            Promise.all(
+              urls.map(async url => {
+                if (unsubscribed) return;
+                try {
+                  await getTransport(url).check(url);
+                  if (unsubscribed) return;
+                  if (!seen[url]) {
+                    seen[url] = 1;
+                    observer.next({ type: "add", descriptor: url });
+                  }
+                } catch (e) {
+                  // nothing
+                  if (seen[url]) {
+                    delete seen[url];
+                    observer.next({ type: "remove", descriptor: url });
+                  }
+                }
+              })
+            )
+          )
           .then(() => new Promise(success => setTimeout(success, 5000)))
           .then(checkLoop);
       }
