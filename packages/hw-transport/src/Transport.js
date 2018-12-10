8dfc2a8619ae51cbb63ab05240ee1ad0aba5d76f
kklash <konnor@exodus.io>
2018-12-10 10:01:59 -0800

    remove flow dependencies and use eslint-standard for hw-transport

--- packages/hw-transport/src/Transport.js
+++ packages/hw-transport/src/Transport.js
@@ -1,35 +1,30 @@
-//@flow
-
-import EventEmitter from "events";
+const EventEmitter = require('events')
 
 /**
- */
-export type Subscription = { unsubscribe: () => void };
 
-/**
- */
-export type Device = Object;
+export type Subscription = { unsubscribe: () => void }
 
-/**
- */
+export type Device = Object
+
 export type DescriptorEvent<Descriptor> = {
-  type: "add" | "remove",
+  type: 'add' | 'remove',
   descriptor: Descriptor,
   device?: Device
-};
-/**
- */
+}
+
 export type Observer<Ev> = $ReadOnly<{
   next: (event: Ev) => mixed,
   error: (e: any) => mixed,
   complete: () => mixed
-}>;
+}>
+
+**/
 
 /**
  * all possible status codes.
  * @see https://github.com/LedgerHQ/blue-app-btc/blob/d8a03d10f77ca5ef8b22a5d062678eef788b824a/include/btchip_apdu_constants.h#L85-L115
  * @example
- * import { StatusCodes } from "@ledgerhq/hw-transport";
+ * import { StatusCodes } from '@ledgerhq/hw-transport'
  */
 export const StatusCodes = {
   PIN_REMAINING_ATTEMPTS: 0x63c0,
@@ -62,24 +57,24 @@ export const StatusCodes = {
   GP_AUTH_FAILED: 0x6300,
   LICENSING: 0x6f42,
   HALTED: 0x6faa
-};
+}
 
-export function getAltStatusMessage(code: number): ?string {
+export function getAltStatusMessage (code) {
   switch (code) {
     // improve text of most common errors
     case 0x6700:
-      return "Incorrect length";
+      return 'Incorrect length'
     case 0x6982:
-      return "Security not satisfied (dongle locked or have invalid access rights)";
+      return 'Security not satisfied (dongle locked or have invalid access rights)'
     case 0x6985:
-      return "Condition of use not satisfied (denied by the user?)";
+      return 'Condition of use not satisfied (denied by the user?)'
     case 0x6a80:
-      return "Invalid data received";
+      return 'Invalid data received'
     case 0x6b00:
-      return "Invalid parameter received";
+      return 'Invalid parameter received'
   }
-  if (0x6f00 <= code && code <= 0x6fff) {
-    return "Internal error, please report";
+  if (code >= 0x6f00 && code <= 0x6fff) {
+    return 'Internal error, please report'
   }
 }
 
@@ -87,47 +82,42 @@ export function getAltStatusMessage(code: number): ?string {
  * TransportError is used for any generic transport errors.
  * e.g. Error thrown when data received by exchanges are incorrect or if exchanged failed to communicate with the device for various reason.
  */
-export function TransportError(message: string, id: string) {
-  this.name = "TransportError";
-  this.message = message;
-  this.stack = new Error().stack;
-  this.id = id;
+export function TransportError (message, id) {
+  this.name = 'TransportError'
+  this.message = message
+  this.stack = new Error().stack
+  this.id = id
 }
-//$FlowFixMe
-TransportError.prototype = new Error();
+TransportError.prototype = new Error()
 
 /**
  * Error thrown when a device returned a non success status.
  * the error.statusCode is one of the `StatusCodes` exported by this library.
  */
-export function TransportStatusError(statusCode: number) {
-  this.name = "TransportStatusError";
-  const statusText =
-    Object.keys(StatusCodes).find(k => StatusCodes[k] === statusCode) ||
-    "UNKNOWN_ERROR";
-  const smsg = getAltStatusMessage(statusCode) || statusText;
-  const statusCodeStr = statusCode.toString(16);
-  this.message = `Ledger device: ${smsg} (0x${statusCodeStr})`;
-  this.stack = new Error().stack;
-  this.statusCode = statusCode;
-  this.statusText = statusText;
+export function TransportStatusError (statusCode) {
+  this.name = 'TransportStatusError'
+  const statusText = Object.keys(StatusCodes).find(k => StatusCodes[k] === statusCode) || 'UNKNOWN_ERROR'
+  const smsg = getAltStatusMessage(statusCode) || statusText
+  const statusCodeStr = statusCode.toString(16)
+  this.message = `Ledger device: ${smsg} (0x${statusCodeStr})`
+  this.stack = new Error().stack
+  this.statusCode = statusCode
+  this.statusText = statusText
 }
-//$FlowFixMe
-TransportStatusError.prototype = new Error();
+TransportStatusError.prototype = new Error()
 
 /**
  * Transport defines the generic interface to share between node/u2f impl
  * A **Descriptor** is a parametric type that is up to be determined for the implementation.
  * it can be for instance an ID, an file path, a URL,...
  */
-export default class Transport<Descriptor> {
-  debug: ?(log: string) => void = global.__ledgerDebug || null;
-  exchangeTimeout: number = 30000;
+module.exports = class Transport {
+  exchangeTimeout = 30000
 
   /**
    * Statically check if a transport is supported on the user's platform/browser.
    */
-  static +isSupported: () => Promise<boolean>;
+  // static +isSupported: () => Promise<boolean>
 
   /**
    * List once all available descriptors. For a better granularity, checkout `listen()`.
@@ -135,11 +125,11 @@ export default class Transport<Descriptor> {
    * @example
    * TransportFoo.list().then(descriptors => ...)
    */
-  static +list: () => Promise<Array<Descriptor>>;
+  // static +list: () => Promise<Array<Descriptor>>
 
   /**
    * Listen all device events for a given Transport. The method takes an Obverver of DescriptorEvent and returns a Subscription (according to Observable paradigm https://github.com/tc39/proposal-observable )
-   * a DescriptorEvent is a `{ descriptor, type }` object. type can be `"add"` or `"remove"` and descriptor is a value you can pass to `open(descriptor)`.
+   * a DescriptorEvent is a `{ descriptor, type }` object. type can be `'add'` or `'remove'` and descriptor is a value you can pass to `open(descriptor)`.
    * each listen() call will first emit all potential device already connected and then will emit events can come over times,
    * for instance if you plug a USB device after listen() or a bluetooth device become discoverable.
    * @param observer is an object with a next, error and complete function (compatible with observer pattern)
@@ -147,9 +137,9 @@ export default class Transport<Descriptor> {
    * @example
 const sub = TransportFoo.listen({
   next: e => {
-    if (e.type==="add") {
-      sub.unsubscribe();
-      const transport = await TransportFoo.open(e.descriptor);
+    if (e.type === 'add') {
+      sub.unsubscribe()
+      const transport = await TransportFoo.open(e.descriptor)
       ...
     }
   },
@@ -157,9 +147,9 @@ const sub = TransportFoo.listen({
   complete: () => {}
 })
    */
-  static +listen: (
-    observer: Observer<DescriptorEvent<Descriptor>>
-  ) => Subscription;
+  // static +listen: (
+  //   observer: Observer<DescriptorEvent<Descriptor>>
+  // ) => Subscription
 
   /**
    * attempt to create a Transport instance with potentially a descriptor.
@@ -169,10 +159,10 @@ const sub = TransportFoo.listen({
    * @example
 TransportFoo.open(descriptor).then(transport => ...)
    */
-  static +open: (
-    descriptor: Descriptor,
-    timeout?: number
-  ) => Promise<Transport<Descriptor>>;
+  // static +open: (
+  //   descriptor: Descriptor,
+  //   timeout?: number
+  // ) => Promise<Transport<Descriptor>>
 
   /**
    * low level api to communicate with the device
@@ -181,60 +171,61 @@ TransportFoo.open(descriptor).then(transport => ...)
    * @param apdu the data to send
    * @return a Promise of response data
    */
-  +exchange: (apdu: Buffer) => Promise<Buffer>;
+  // +exchange: (apdu: Buffer) => Promise<Buffer>
 
   /**
-   * set the "scramble key" for the next exchanges with the device.
+   * set the 'scramble key' for the next exchanges with the device.
    * Each App can have a different scramble key and they internally will set it at instanciation.
    * @param key the scramble key
    */
-  +setScrambleKey: (key: string) => void;
+  // +setScrambleKey: (key: string) => void
 
   /**
    * close the exchange with the device.
    * @return a Promise that ends when the transport is closed.
    */
-  +close: () => Promise<void>;
+  // +close: () => Promise<void>
 
-  _events = new EventEmitter();
+  _events = new EventEmitter()
 
   /**
    * Listen to an event on an instance of transport.
    * Transport implementation can have specific events. Here is the common events:
-   * * `"disconnect"` : triggered if Transport is disconnected
+   * * `'disconnect'` : triggered if Transport is disconnected
    */
-  on(eventName: string, cb: Function) {
-    this._events.on(eventName, cb);
+  on (eventName, cb) {
+    this._events.on(eventName, cb)
   }
 
   /**
    * Stop listening to an event on an instance of transport.
    */
-  off(eventName: string, cb: Function) {
-    this._events.removeListener(eventName, cb);
+  off (eventName, cb) {
+    this._events.removeListener(eventName, cb)
   }
 
-  emit(event: string, ...args: *) {
-    this._events.emit(event, ...args);
+  emit (event, ...args) {
+    this._events.emit(event, ...args)
   }
 
   /**
    * Enable or not logs of the binary exchange
    */
-  setDebugMode(debug: boolean | ((log: string) => void)) {
-    this.debug =
-      typeof debug === "function"
-        ? debug
-        : debug
-          ? log => console.log(log)
-          : null;
+  setDebugMode (debug) {
+    if (typeof debug === 'function') {
+      this.debug = debug
+    } else if (debug) {
+      this.debug = (msg) => console.log(msg)
+    } else {
+      this.debug = null
+    }
   }
 
   /**
    * Set a timeout (in milliseconds) for the exchange call. Only some transport might implement it. (e.g. U2F)
    */
-  setExchangeTimeout(exchangeTimeout: number) {
-    this.exchangeTimeout = exchangeTimeout;
+  setExchangeTimeout (exchangeTimeout) {
+    this.exchangeTimeout = exchangeTimeout
   }
 
   /**
@@ -247,19 +238,12 @@ TransportFoo.open(descriptor).then(transport => ...)
    * @param statusList is a list of accepted status code (shorts). [0x9000] by default
    * @return a Promise of response buffer
    */
-  send = async (
-    cla: number,
-    ins: number,
-    p1: number,
-    p2: number,
-    data: Buffer = Buffer.alloc(0),
-    statusList: Array<number> = [StatusCodes.OK]
-  ): Promise<Buffer> => {
+  async send (cla, ins, p1, p2, data = Buffer.alloc(0), statusList = [StatusCodes.OK]) {
     if (data.length >= 256) {
       throw new TransportError(
-        "data.length exceed 256 bytes limit. Got: " + data.length,
-        "DataLengthTooBig"
-      );
+        'data.length exceed 256 bytes limit. Got: ' + data.length,
+        'DataLengthTooBig'
+      )
     }
     const response = await this.exchange(
       Buffer.concat([
@@ -267,13 +251,13 @@ TransportFoo.open(descriptor).then(transport => ...)
         Buffer.from([data.length]),
         data
       ])
-    );
-    const sw = response.readUInt16BE(response.length - 2);
+    )
+    const sw = response.readUInt16BE(response.length - 2)
     if (!statusList.some(s => s === sw)) {
-      throw new TransportStatusError(sw);
+      throw new TransportStatusError(sw)
     }
-    return response;
-  };
+    return response
+  }
 
   /**
    * create() allows to open the first descriptor available or
@@ -282,89 +266,80 @@ TransportFoo.open(descriptor).then(transport => ...)
    * @example
 TransportFoo.create().then(transport => ...)
    */
-  static create(
-    openTimeout?: number = 3000,
-    listenTimeout?: number = 10000
-  ): Promise<Transport<Descriptor>> {
+  static async create (openTimeout = 3000, listenTimeout = 10000) {
     return new Promise((resolve, reject) => {
-      let found = false;
+      let found = false
       const sub = this.listen({
         next: e => {
-          found = true;
-          if (sub) sub.unsubscribe();
-          clearTimeout(listenTimeoutId);
-          this.open(e.descriptor, openTimeout).then(resolve, reject);
+          found = true
+          if (sub) sub.unsubscribe()
+          clearTimeout(listenTimeoutId)
+          this.open(e.descriptor, openTimeout).then(resolve, reject)
         },
         error: e => {
-          clearTimeout(listenTimeoutId);
-          reject(e);
+          clearTimeout(listenTimeoutId)
+          reject(e)
         },
         complete: () => {
-          clearTimeout(listenTimeoutId);
+          clearTimeout(listenTimeoutId)
           if (!found) {
-            reject(
-              new TransportError(
-                this.ErrorMessage_NoDeviceFound,
-                "NoDeviceFound"
-              )
-            );
+            reject(new TransportError(
+              this.ErrorMessageNoDeviceFound,
+              'NoDeviceFound'
+            ))
           }
         }
-      });
+      })
       const listenTimeoutId = setTimeout(() => {
-        sub.unsubscribe();
+        sub.unsubscribe()
         reject(
-          new TransportError(this.ErrorMessage_ListenTimeout, "ListenTimeout")
-        );
-      }, listenTimeout);
-    });
+          new TransportError(this.ErrorMessageListenTimeout, 'ListenTimeout')
+        )
+      }, listenTimeout)
+    })
   }
 
-  decorateAppAPIMethods(
-    self: Object,
-    methods: Array<string>,
-    scrambleKey: string
-  ) {
+  decorateAppAPIMethods (self, methods, scrambleKey) {
     for (let methodName of methods) {
       self[methodName] = this.decorateAppAPIMethod(
         methodName,
         self[methodName],
         self,
         scrambleKey
-      );
+      )
     }
   }
 
-  _appAPIlock = null;
-  decorateAppAPIMethod<R, A: any[]>(
+  _appAPIlock = null
+  decorateAppAPIMethod (
     methodName: string,
-    f: (...args: A) => Promise<R>,
-    ctx: *,
-    scrambleKey: string
-  ): (...args: A) => Promise<R> {
+    f,
+    ctx,
+    scrambleKey
+  ) {
     return async (...args) => {
-      const { _appAPIlock } = this;
+      const { _appAPIlock } = this
       if (_appAPIlock) {
         const e = new TransportError(
-          "Ledger Device is busy (lock " + _appAPIlock + ")",
-          "TransportLocked"
-        );
+          'Ledger Device is busy (lock ' + _appAPIlock + ')',
+          'TransportLocked'
+        )
         Object.assign(e, {
           currentLock: _appAPIlock,
           methodName
-        });
-        return Promise.reject(e);
+        })
+        return Promise.reject(e)
       }
       try {
-        this._appAPIlock = methodName;
-        this.setScrambleKey(scrambleKey);
-        return await f.apply(ctx, args);
+        this._appAPIlock = methodName
+        this.setScrambleKey(scrambleKey)
+        return await f.apply(ctx, args)
       } finally {
-        this._appAPIlock = null;
+        this._appAPIlock = null
       }
-    };
+    }
   }
 
-  static ErrorMessage_ListenTimeout = "No Ledger device found (timeout)";
-  static ErrorMessage_NoDeviceFound = "No Ledger device found";
+  static ErrorMessageListenTimeout = 'No Ledger device found (timeout)'
+  static ErrorMessageNoDeviceFound = 'No Ledger device found'
 }
