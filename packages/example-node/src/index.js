e29edbc3c0411c26a5333380be9849f6e7501f65
Jozef Latka <jozef.latka@hotmail.com>
2019-01-04 16:16:04 +0100

    Made it work

--- packages/example-node/src/index.js
+++ packages/example-node/src/index.js
@@ -1,12 +1,20 @@
 import TransportNodeHid from "@ledgerhq/hw-transport-node-hid";
-import AppBtc from "@ledgerhq/hw-app-btc";
+import AppAda from "@ledgerhq/hw-app-ada";
 
 async function example() {
   const transport = await TransportNodeHid.create(5000);
   transport.setDebugMode(true);
-  const appBtc = new AppBtc(transport);
-  const o = await appBtc.getWalletPublicKey("44'/0'/0'/0");
-  console.log("getWalletPublicKey:", o);
+  const appAda = new AppAda(transport);
+
+  // console.log(
+  //   await appAda.getExtendedPublicKey([
+  //     0x80000000 + 44,
+  //     0x80000000 + 1815,
+  //     0x80000000 + 1
+  //   ])
+  // );
+
+  console.log(await appAda.getVersion());
 }
 
 example();
