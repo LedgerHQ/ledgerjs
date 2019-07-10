c57ce0e2e3ff2172e635731d6ddcac05ec8548d9
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2019-07-10 12:43:22 +0200

    Fixes HID transport to Promise.reject errors instead of throwing them

--- packages/hw-transport-node-hid-noevents/src/TransportNodeHid.js
+++ packages/hw-transport-node-hid-noevents/src/TransportNodeHid.js
@@ -69,15 +69,15 @@ export default class TransportNodeHidNoEvents extends Transport<?string> {
   /**
    * if path="" is not provided, the library will take the first device
    */
-  static async open(path: ?string) {
-    if (path) {
-      return Promise.resolve(new TransportNodeHidNoEvents(new HID.HID(path)));
-    }
-    const device = getDevices()[0];
-    if (!device) throw new TransportError("NoDevice", "NoDevice");
-    return Promise.resolve(
-      new TransportNodeHidNoEvents(new HID.HID(device.path))
-    );
+  static open(path: ?string) {
+    return Promise.resolve().then(() => {
+      if (path) {
+        return new TransportNodeHidNoEvents(new HID.HID(path));
+      }
+      const device = getDevices()[0];
+      if (!device) throw new TransportError("NoDevice", "NoDevice");
+      return new TransportNodeHidNoEvents(new HID.HID(device.path));
+    });
   }
 
   device: HID.HID;
