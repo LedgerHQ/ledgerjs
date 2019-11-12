bf832d32e96c201e658294689ee25f58d405e403
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2019-11-12 20:11:16 +0100

    prettier

--- packages/errors/src/index.js
+++ packages/errors/src/index.js
@@ -106,7 +106,9 @@ export const ManagerUninstallBTCDep = createCustomErrorClass(
 export const NetworkDown = createCustomErrorClass("NetworkDown");
 export const NoAddressesFound = createCustomErrorClass("NoAddressesFound");
 export const NotEnoughBalance = createCustomErrorClass("NotEnoughBalance");
-export const NotEnoughSpendableBalance = createCustomErrorClass("NotEnoughSpendableBalance")
+export const NotEnoughSpendableBalance = createCustomErrorClass(
+  "NotEnoughSpendableBalance"
+);
 export const NotEnoughBalanceBecauseDestinationNotCreated = createCustomErrorClass(
   "NotEnoughBalanceBecauseDestinationNotCreated"
 );
