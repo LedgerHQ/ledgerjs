9ad0400f60b5dabaf1c52a9055f3c09fad88f7ee
BTChip github <contact@btchip.com>
2020-02-23 17:20:40 +0100

    Decorate

--- packages/hw-app-eth/src/Eth.js
+++ packages/hw-app-eth/src/Eth.js
@@ -53,7 +53,8 @@ export default class Eth {
         "getAppConfiguration",
         "starkGetPublicKey",
         "starkSignOrder",
-        "starkSignTransfer"
+        "starkSignTransfer",
+        "starkProvideQuantum"
       ],
       scrambleKey
     );
