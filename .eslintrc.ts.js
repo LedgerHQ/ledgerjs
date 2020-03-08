8d321900e81e71428d1f97c5919cebb430f6cd3c
jsiguira <jun@dappface.com>
2020-03-08 13:21:08 +0100

    setup linter for ts

---
+++ .eslintrc.ts.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
+  ],
+  rules: {
+    "no-console": 0,
+    "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "flowtype/space-after-type-colon": [2, "always", { allowLineBreak: true }],
+    "prettier/prettier": "error"
+  },
+  settings: {
+    flowtype: {
+      onlyFilesWithFlowAnnotation: true
+    }
+  }
+};
