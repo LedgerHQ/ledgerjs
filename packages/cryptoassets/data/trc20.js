e06d834ebad142dbc4e60420ad937761e59a53bc
FabriceDautriat <fabrice.dautriat@ledger.fr>
2021-03-30 15:14:57 +0200

    Add SEED TRC20

--- packages/cryptoassets/data/trc20.js
+++ packages/cryptoassets/data/trc20.js
@@ -3,4 +3,5 @@ module.exports = [
   ["TLa2f6VPqDgRE67v1736s7bJ8Ray5wYjU7","WIN","WINK","TLa2f6VPqDgRE67v1736s7bJ8Ray5wYjU7",6],
   ["TCFLL5dx5ZJdKnWuesXxi1VPwjLVmWZZy9","JST","JUST GOV","TCFLL5dx5ZJdKnWuesXxi1VPwjLVmWZZy9",18],
   ["TMwFHYXLJaRUPeW6421aqXL4ZEzPRFGkGT","USDJ","JUST Stablecoin","TMwFHYXLJaRUPeW6421aqXL4ZEzPRFGkGT",18]
+  ["TBwoSTyywvLrgjSgaatxrBhxt3DGpVuENh","SEED","SEED","TBwoSTyywvLrgjSgaatxrBhxt3DGpVuENh",6]
 ];
