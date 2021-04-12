0eb04ae028c6dc54e459f1fd80efda8724925ca1
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2021-04-12 10:59:27 +0200

    Fixes #584 add SEED trc20

--- packages/cryptoassets/data/trc20.js
+++ packages/cryptoassets/data/trc20.js
@@ -3,5 +3,6 @@ module.exports = [
   ["TLa2f6VPqDgRE67v1736s7bJ8Ray5wYjU7","WIN","WINK","TLa2f6VPqDgRE67v1736s7bJ8Ray5wYjU7",6],
   ["TCFLL5dx5ZJdKnWuesXxi1VPwjLVmWZZy9","JST","JUST GOV","TCFLL5dx5ZJdKnWuesXxi1VPwjLVmWZZy9",18],
   ["TMwFHYXLJaRUPeW6421aqXL4ZEzPRFGkGT","USDJ","JUST Stablecoin","TMwFHYXLJaRUPeW6421aqXL4ZEzPRFGkGT",18],
-  ["TVj7RNVHy6thbM7BWdSe9G6gXwKhjhdNZS","KLV","Klever","TVj7RNVHy6thbM7BWdSe9G6gXwKhjhdNZS",6]
+  ["TVj7RNVHy6thbM7BWdSe9G6gXwKhjhdNZS","KLV","Klever","TVj7RNVHy6thbM7BWdSe9G6gXwKhjhdNZS",6],
+  ["TBwoSTyywvLrgjSgaatxrBhxt3DGpVuENh","SEED","SEED","TBwoSTyywvLrgjSgaatxrBhxt3DGpVuENh",6]
 ];
