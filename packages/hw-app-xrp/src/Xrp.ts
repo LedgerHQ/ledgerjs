33633a514fed89ad3b2c023e59fc54ec1ed827dc
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2021-05-06 15:14:38 +0200

    convert hw-app-xrp to ts

--- packages/hw-app-xrp/src/Xrp.ts
+++ packages/hw-app-xrp/src/Xrp.ts
@@ -1,8 +1,5 @@
-//@flow
-
 import type Transport from "@ledgerhq/hw-transport";
 import BIPPath from "bip32-path";
-
 /**
  * XRP API
  *
@@ -49,10 +46,11 @@ import BIPPath from "bip32-path";
  *     .then(signature => console.log(`Signature: ${signature}`))
  *     .catch(e => console.log(`An error occurred (${e.message})`));
  */
-export default class Xrp {
-  transport: Transport<*>;
 
-  constructor(transport: Transport<*>, scrambleKey: string = "XRP") {
+export default class Xrp {
+  transport: Transport<any>;
+
+  constructor(transport: Transport<any>, scrambleKey = "XRP") {
     this.transport = transport;
     transport.decorateAppAPIMethods(
       this,
@@ -79,46 +77,39 @@ export default class Xrp {
     chainCode?: boolean,
     ed25519?: boolean
   ): Promise<{
-    publicKey: string,
-    address: string,
-    chainCode?: string,
+    publicKey: string;
+    address: string;
+    chainCode?: string;
   }> {
     const bipPath = BIPPath.fromString(path).toPathArray();
     const curveMask = ed25519 ? 0x80 : 0x40;
-
     const cla = 0xe0;
     const ins = 0x02;
     const p1 = display ? 0x01 : 0x00;
     const p2 = curveMask | (chainCode ? 0x01 : 0x00);
     const data = Buffer.alloc(1 + bipPath.length * 4);
-
     data.writeInt8(bipPath.length, 0);
     bipPath.forEach((segment, index) => {
       data.writeUInt32BE(segment, 1 + index * 4);
     });
-
     const response = await this.transport.send(cla, ins, p1, p2, data);
-
-    const result = {};
     const publicKeyLength = response[0];
     const addressLength = response[1 + publicKeyLength];
 
-    result.publicKey = response.slice(1, 1 + publicKeyLength).toString("hex");
-
-    result.address = response
-      .slice(1 + publicKeyLength + 1, 1 + publicKeyLength + 1 + addressLength)
-      .toString("ascii");
-
-    if (chainCode) {
-      result.chainCode = response
-        .slice(
-          1 + publicKeyLength + 1 + addressLength,
-          1 + publicKeyLength + 1 + addressLength + 32
-        )
-        .toString("hex");
-    }
-
-    return result;
+    return {
+      publicKey: response.slice(1, 1 + publicKeyLength).toString("hex"),
+      address: response
+        .slice(1 + publicKeyLength + 1, 1 + publicKeyLength + 1 + addressLength)
+        .toString("ascii"),
+      chainCode: chainCode
+        ? response
+            .slice(
+              1 + publicKeyLength + 1 + addressLength,
+              1 + publicKeyLength + 1 + addressLength + 32
+            )
+            .toString("hex")
+        : undefined,
+    };
   }
 
   /**
@@ -144,17 +135,20 @@ export default class Xrp {
     const bipPath = BIPPath.fromString(path).toPathArray();
     const rawTx = Buffer.from(rawTxHex, "hex");
     const curveMask = ed25519 ? 0x80 : 0x40;
-
-    const apdus = [];
+    const apdus: {
+      cla: number;
+      ins: number;
+      p1: number;
+      p2: number;
+      data: Buffer;
+    }[] = [];
     let offset = 0;
 
     while (offset !== rawTx.length) {
       const isFirst = offset === 0;
       const maxChunkSize = isFirst ? 150 - 1 - bipPath.length * 4 : 150;
-
       const hasMore = offset + maxChunkSize < rawTx.length;
       const chunkSize = hasMore ? maxChunkSize : rawTx.length - offset;
-
       const apdu = {
         cla: 0xe0,
         ins: 0x04,
@@ -185,7 +179,8 @@ export default class Xrp {
     }
 
     let response = Buffer.alloc(0);
-    for (let apdu of apdus) {
+
+    for (const apdu of apdus) {
       response = await this.transport.send(
         apdu.cla,
         apdu.ins,
@@ -211,11 +206,11 @@ export default class Xrp {
    * }
    */
   async getAppConfiguration(): Promise<{
-    version: string,
+    version: string;
   }> {
     const response = await this.transport.send(0xe0, 0x06, 0x00, 0x00);
-    const result = {};
-    result.version = "" + response[1] + "." + response[2] + "." + response[3];
-    return result;
+    return {
+      version: "" + response[1] + "." + response[2] + "." + response[3],
+    };
   }
 }
