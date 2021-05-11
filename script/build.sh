381c8ccbb46ddcd485f13465a7eac699eea0740f
JunichiSugiura <jun.sugiura.jp@gmail.com>
2021-05-11 10:51:07 +0200

    move build script back to shell file

--- script/build.sh
+++ script/build.sh
@@ -5,8 +5,6 @@ set -e
 cd ../..
 PATH=$(yarn bin):$PATH
 cd -
+
 export NODE_ENV=production
-BABEL_ENV=cjs babel --source-maps -d lib src --config-file ../../babel.config.js
-flow-copy-source -v src lib
-BABEL_ENV=es babel --source-maps -d lib-es src --config-file ../../babel.config.js
-flow-copy-source -v src lib-es
+tsc && tsc -m ES6 --outDir lib-es
