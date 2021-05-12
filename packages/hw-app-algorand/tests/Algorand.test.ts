d5a516981a59024a3bad4a46b654ec1a21fa2392
JunichiSugiura <jun.sugiura.jp@gmail.com>
2021-05-12 10:44:17 +0200

    convert Algorand test

--- packages/hw-app-algorand/tests/Algorand.test.ts
+++ packages/hw-app-algorand/tests/Algorand.test.ts
@@ -6,6 +6,7 @@ import Algorand from "../src/Algorand";
 
 test("Algorand init", async () => {
   const Transport = createTransportReplayer(RecordStore.fromString(""));
+  // @ts-expect-error
   const transport = await Transport.open();
   const algo = new Algorand(transport);
   expect(algo).not.toBe(undefined);
