413f877910fc7bde35d519e40087350296b4ec16
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2021-05-12 17:09:51 +0200

    Tezos tests to ts

--- packages/hw-app-tezos/tests/Tezos.test.ts
+++ packages/hw-app-tezos/tests/Tezos.test.ts
@@ -6,6 +6,7 @@ import Tezos from "../src/Tezos";
 
 test("Tezos init", async () => {
   const Transport = createTransportReplayer(RecordStore.fromString(""));
+  // @ts-expect-error
   const transport = await Transport.open();
   const xtz = new Tezos(transport);
   expect(xtz).not.toBe(undefined);
