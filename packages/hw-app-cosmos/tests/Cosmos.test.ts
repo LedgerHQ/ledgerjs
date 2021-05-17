be1864f97b349a5d6d7f152436b38884039df928
JunichiSugiura <jun.sugiura.jp@gmail.com>
2021-05-17 08:57:16 +0200

    convert hw-app-cosmos test

--- packages/hw-app-cosmos/tests/Cosmos.test.ts
+++ packages/hw-app-cosmos/tests/Cosmos.test.ts
@@ -6,6 +6,7 @@ import Cosmos from "../src/Cosmos";
 
 test("Comos init", async () => {
   const Transport = createTransportReplayer(RecordStore.fromString(""));
+  // @ts-expect-error Todo: fix types for Transport creator
   const transport = await Transport.open();
   const cosmos = new Cosmos(transport);
   expect(cosmos).not.toBe(undefined);
