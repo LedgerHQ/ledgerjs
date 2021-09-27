75632f6f66184e1fd38d7fc9afca0a9964d14ad3
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2021-09-27 14:51:22 +0200

    LL-7448 add bsc plugins support

--- script/crypto-assets-importer/importers/ethereum-plugins.js
+++ script/crypto-assets-importer/importers/ethereum-plugins.js
@@ -4,7 +4,7 @@ const { readFileJSON } = require("../utils");
 const mapObject = (obj, fn) => Object.fromEntries(Object.entries(obj).map(fn));
 
 module.exports = {
-  paths: ["dapps/ethereum"],
+  paths: ["dapps/ethereum", "dapps/bsc"],
   output: "ethereum.json", // to be put in crypto assets list
   outputTemplate: (data) =>
     JSON.stringify(
