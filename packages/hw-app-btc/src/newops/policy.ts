bacdc9955659e055dd14039f070d7fa9ecd27332
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2021-10-01 14:38:25 +0200

    Draft of application client; added some exports, and fixed various compilation issues

--- packages/hw-app-btc/src/newops/policy.ts
+++ packages/hw-app-btc/src/newops/policy.ts
@@ -2,6 +2,7 @@ import { pathArrayToString } from "../bip32";
 import { BufferWriter } from "../buffertools";
 import { crypto } from "bitcoinjs-lib";
 import { Merkle } from "./merkle";
+import { createVarint } from "../varint";
 
 export type DefaultDescriptorTemplate = "pkh(@0)" | "sh(wpkh(@0))" | "wpkh(@0)" | "tr(@0)";
 
