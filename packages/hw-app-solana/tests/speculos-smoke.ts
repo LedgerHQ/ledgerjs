e084e58351153bcb5f0f8e534c7c627cc694380f
konoart <konoart@solana.com>
2021-10-07 09:46:41 +0200

    switch to automation api for smoke test

--- packages/hw-app-solana/tests/speculos-smoke.ts
+++ packages/hw-app-solana/tests/speculos-smoke.ts
@@ -1,22 +1,15 @@
 import SpeculosTransport from "@ledgerhq/hw-transport-node-speculos";
 import Solana from "../src/Solana";
 
-import { Observable } from "rxjs";
-import { share, filter, take, timeout } from "rxjs/operators";
-
-import http from "http";
+import axios from "axios";
 
 const SPECULOS_APDU_PORT = 9999;
 const SPECULOS_REST_API_ENDPOINT = "http://0.0.0.0:5000";
 
-(async () => {
+async function runSmokeTest() {
     const speculosTransport = await SpeculosTransport.open({
         apduPort: SPECULOS_APDU_PORT,
     });
-
-    const displayStates = speculosDisplayStates().pipe(share());
-    const keepAliveDisplayStates = displayStates.subscribe(() => {});
-    const waitDisplayText = speculosDisplayTextWaiter(displayStates);
 
     try {
         const solana = new Solana(speculosTransport);
@@ -38,107 +31,51 @@ const SPECULOS_REST_API_ENDPOINT = "http://0.0.0.0:5000";
         );
         const signaturePromise = solana.signTransaction("44'/501'", txBuffer);
 
-        await waitDisplayText("Transfer");
-
-        await pressSpeculosButtons(["right", 4], "both");
+        await approveTransaction();
 
         const signatureResult = await signaturePromise;
 
         console.log("sign transaction", {
             signature: signatureResult.signature.toString("hex"),
         });
-    } catch (e) {
-        console.error(e);
     } finally {
         speculosTransport.close();
-        keepAliveDisplayStates.unsubscribe();
-    }
-})();
-
-function speculosDisplayTextWaiter(
-    speculosStates: Observable<SpeculosDisplayState>
-) {
-    return (text: string) => {
-        console.log(`Waiting for display text: ${text}...`);
-        return speculosStates
-            .pipe(
-                filter((state) => state.text === text),
-                take(1),
-                timeout(10 * 1000)
-            )
-            .toPromise();
-    };
-}
-
-type Button = "left" | "right" | "both";
-
-function pressSpeculosButton(button: Button) {
-    const payload = {
-        action: "press-and-release",
-    };
-
-    const data = new TextEncoder().encode(JSON.stringify(payload));
-
-    return new Promise<void>((resolve, reject) => {
-        const req = http.request(
-            `${SPECULOS_REST_API_ENDPOINT}/button/${button}`,
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Content-Length": data.length,
-                },
-            },
-            (res) => {
-                return res.statusCode === 200 ? resolve() : reject(res);
-            }
-        );
-
-        req.write(data);
-        req.end();
-    });
-}
-
-async function pressSpeculosButtons(...buttons: ([Button, number] | Button)[]) {
-    for (const buttonSpec of buttons) {
-        const [button, times] =
-            typeof buttonSpec === "string" ? [buttonSpec, 1] : buttonSpec;
-        for (const _ of Array(times).keys()) {
-            await pressSpeculosButton(button);
-        }
     }
 }
 
-function speculosDisplayStates(): Observable<SpeculosDisplayState> {
-    return new Observable((subsriber) => {
-        const req = http.request(
-            `${SPECULOS_REST_API_ENDPOINT}/events?stream=true`,
-            (res) => {
-                res.setEncoding("utf8");
-                res.on("data", (eventData: string) => {
-                    for (const line of eventData
-                        .split("\n")
-                        .filter((line) => line.length > 0)) {
-                        // prefix is: "data: "
-                        const event = JSON.parse(
-                            line.substring(6)
-                        ) as SpeculosDisplayState;
-                        subsriber.next(event);
-                    }
-                });
-            }
-        );
-
-        req.end();
-
-        return () => {
-            req.destroy();
-        };
-    });
+function approveTransaction() {
+    return axios.post(
+        `${SPECULOS_REST_API_ENDPOINT}/automation`,
+        approveTransactionFlow
+    );
 }
 
-type SpeculosDisplayState = {
-    text: string;
-    x: number;
-    y: number;
+const approveTransactionFlow = {
+    version: 1,
+    rules: [
+        {
+            text: "Transfer",
+            //conditions: [["seen", false]],
+            actions: [
+                ["button", 2, true],
+                ["button", 2, false],
+                ["button", 2, true],
+                ["button", 2, false],
+                ["button", 2, true],
+                ["button", 2, false],
+                ["button", 2, true],
+                ["button", 2, false],
+                ["button", 1, true],
+                ["button", 2, true],
+                ["button", 1, false],
+                ["button", 2, false],
+                //["setbool", "seen", true],
+            ],
+        },
+    ],
 };
+
+runSmokeTest().catch((e) => {
+    console.error(e);
+    process.exit(1);
+});
