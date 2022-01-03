46de1c7062c79d3941c0bcdb24b84e903f7d4dee
Andrew Allen <andrew@allenan.com>
2022-01-03 01:36:16 -0800

    add helium asset info to cryptoassets (#724)

--- packages/cryptoassets/src/currencies.ts
+++ packages/cryptoassets/src/currencies.ts
@@ -1243,6 +1243,35 @@ const cryptocurrenciesById: Record<string, CryptoCurrency> = {
       },
     ],
   },
+  helium: {
+    type: "CryptoCurrency",
+    id: "helium",
+    coinType: 904,
+    name: "Helium",
+    managerAppName: "Helium",
+    ticker: "HNT",
+    scheme: "helium",
+    color: "#474DFF",
+    family: "helium",
+    units: [
+      {
+        name: "HNT",
+        code: "HNT",
+        magnitude: 8,
+      },
+      {
+        name: "bones",
+        code: "bones",
+        magnitude: 0,
+      },
+    ],
+    explorerViews: [
+      {
+        tx: "https://explorer.helium.com/txns/$hash",
+        address: "https://explorer.helium.com/accounts/$address",
+      },
+    ],
+  },
   hpb: {
     type: "CryptoCurrency",
     id: "hpb",
