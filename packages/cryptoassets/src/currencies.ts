ac5f6028dd15e582a3ebc5b3ee8e47ed3f04237b
Marco - Figment <98154129+marco-figment@users.noreply.github.com>
2022-02-14 14:26:01 +0100

    Add Osmosis currency definition

--- packages/cryptoassets/src/currencies.ts
+++ packages/cryptoassets/src/currencies.ts
@@ -2504,6 +2504,35 @@ const cryptocurrenciesById: Record<string, CryptoCurrency> = {
       },
     ],
   },
+  osmosis: {
+    type: "CryptoCurrency",
+    id: "osmo",
+    coinType: 118,
+    name: "Osmosis",
+    managerAppName: "Cosmos",
+    ticker: "OSMO",
+    scheme: "osmo",
+    color: "#493c9b",
+    family: "osmosis",
+    units: [
+      {
+        name: "Osmosis",
+        code: "OSMO",
+        magnitude: 6,
+      },
+      {
+        name: "Micro-OSMO",
+        code: "uosmo",
+        magnitude: 0,
+      },
+    ],
+    explorerViews: [
+      {
+        tx: "https://www.mintscan.io/osmosis/txs/$hash",
+        address: "https://www.mintscan.io/osmosis/account/$address",
+      },
+    ],
+  },
   tezos: {
     type: "CryptoCurrency",
     id: "tezos",
