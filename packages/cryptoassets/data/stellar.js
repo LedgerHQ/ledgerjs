fbac8a494cb68b011b8099b4b8ecb6c86e2f0b8b
Iveta <quietbits@gmail.com>
2022-02-17 10:44:24 -0500

    Stellar: add comment

--- packages/cryptoassets/data/stellar.js
+++ packages/cryptoassets/data/stellar.js
@@ -1,3 +1,4 @@
 module.exports = [
+  // [assetCode, assetIssuer, assetType, name, precision, enableCountervalues]
   ["USDC","GA5ZSEJYB37JRC5AVCIA5MOP4RHTM335X2KGX3IHOJAPP5RE34K4KZVN", "credit_alphanum4", "USDC", 7, true],
 ]
