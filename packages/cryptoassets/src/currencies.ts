54d2acfb69253aecdbf4e0440207d4860480689c
Di Wu <dw323@cornell.edu>
2022-04-28 17:48:11 -0700

    add shyft network

--- packages/cryptoassets/src/currencies.ts
+++ packages/cryptoassets/src/currencies.ts
@@ -2532,6 +2532,31 @@ const cryptocurrenciesById: Record<string, CryptoCurrency> = {
       },
     ],
   },
+  shyft: {
+    type: "CryptoCurrency",
+    id: "shyft",
+    coinType: 7341,
+    name: "Shyft",
+    managerAppName: "Shyft",
+    ticker: "SHFT",
+    scheme: "shfyt",
+    color: "#662c5e",
+    family: "shyft",
+    blockAvgTime: 5,
+    units: [
+      {
+        name: "SHFT",
+        code: "SHFT",
+        magnitude: 18,
+      },
+    ],
+    explorerViews: [
+      {
+        tx: "https://bx.shyft.network/tx/$hash",
+        address: "https://bx.shyft.network/address/$address",
+      },
+    ],
+  },
   tezos: {
     type: "CryptoCurrency",
     id: "tezos",
